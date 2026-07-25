1784392c51f9fa9db7ef32bbf488c6cfb965676d
Bhanu Bhattarai <bhanubhattarai13@gmail.com>
2026-07-26 04:09:51 +0530

    add splahscreen style

--- app/driverApp/src/main/kotlin/com/taxibou/driver/DriverMainActivity.kt
+++ app/driverApp/src/main/kotlin/com/taxibou/driver/DriverMainActivity.kt
@@ -2,15 +2,21 @@ package com.taxibou.driver
 
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.ui.Modifier
 import com.taxibou.app.MainActivity
+import com.taxibou.ui.TaxibouTheme
 
 class DriverMainActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme() { }
+        setAppComposable {
+            TaxibouTheme(it, lightColorScheme(), darkColorScheme()) {
+                Surface(Modifier.fillMaxSize()) { }
+            }
         }
     }
 }
--- app/riderApp/src/main/kotlin/com/taxibou/rider/RiderMainActivity.kt
+++ app/riderApp/src/main/kotlin/com/taxibou/rider/RiderMainActivity.kt
@@ -1,22 +1,12 @@
 package com.taxibou.rider
 
 import android.os.Bundle
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import com.taxibou.UiMode
 import com.taxibou.app.MainActivity
 import com.taxibou.ui.TaxibouTheme
@@ -33,19 +23,7 @@ class RiderMainActivity : MainActivity() {
 @Composable
 fun RiderApp(uiMode: UiMode) {
     TaxibouTheme(uiMode, lightColorScheme(), darkColorScheme()) {
-        Box(Modifier.fillMaxSize()) {
-            var offset by remember { mutableFloatStateOf(0f) }
-            Canvas(Modifier.fillMaxSize()) {
-                val spawnPoint = Offset(size.width / 2, 0f + offset)
-                drawCircle(Color.Black, radius = 100f, center = spawnPoint)
-            }
-            Button({
-
-                offset += 10f
-            }, Modifier.align(Alignment.Center)) {
-                Text("Click me")
-            }
-        }
+        Surface(Modifier.fillMaxSize()) { }
     }
 
 }
